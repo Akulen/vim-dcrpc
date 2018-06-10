5278190e9c2c1af664964685999f1adda489b5c6
Arda Ünlü <aarda.uunlu@gmail.com>
2018-06-11 01:01:52 +0300

    simpler file

--- vim-dcrpc.c
+++ vim-dcrpc.c
@@ -6,48 +6,24 @@
 #include "include/discord_rpc.h"
 
 const char* APPLICATION_ID = "455442010573111308";
-int SendPresence = 1;
 
 static void updateDiscordPresence(int linenum, int curline, char* filename) {
-    if (SendPresence) {
-        char buffer[2][256];
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        sprintf(buffer[0], "Line: %d of %d", curline, linenum);
-        discordPresence.state = buffer[0];
-        sprintf(buffer[1], "Editing %s", filename);
-        discordPresence.details = buffer[1];
-        discordPresence.largeImageKey = "vim_logo";
-        //discordPresence.smallImageKey = "vim_logo_s";
-        discordPresence.instance = 0;
-        Discord_UpdatePresence(&discordPresence);
-    }
-    else {
-        Discord_ClearPresence();
-    }
-}
-
-static void handleDiscordReady(const DiscordUser* connectedUser) {
-    printf("\nDiscord: connected to user %s#%s - %s\n",
-           connectedUser->username,
-           connectedUser->discriminator,
-           connectedUser->userId);
-}
-
-static void handleDiscordDisconnected(int errcode, const char* message) {
-    printf("\nDiscord: disconnected (%d: %s)\n", errcode, message);
-}
-
-static void handleDiscordError(int errcode, const char* message) {
-    printf("\nDiscord: error (%d: %s)\n", errcode, message);
+    char buffer[2][256];
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    sprintf(buffer[0], "Line: %d of %d", curline, linenum);
+    discordPresence.state = buffer[0];
+    sprintf(buffer[1], "Editing %s", filename);
+    discordPresence.details = buffer[1];
+    discordPresence.largeImageKey = "vim_logo";
+    //discordPresence.smallImageKey = "vim_logo_s";
+    discordPresence.instance = 0;
+    Discord_UpdatePresence(&discordPresence);
 }
 
 static void discordInit() {
     DiscordEventHandlers handlers;
     memset(&handlers, 0, sizeof(handlers));
-    handlers.ready = handleDiscordReady;
-    handlers.disconnected = handleDiscordDisconnected;
-    handlers.errored = handleDiscordError;
     Discord_Initialize(APPLICATION_ID, &handlers, 1, NULL);
 }
 
